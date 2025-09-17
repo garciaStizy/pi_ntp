81981be1629f58e78d87d2b4414a5be06ec537ec
Samuel <samuelbernal0809@gmail.com>
2025-09-17 12:08:18 -0500

    agregar los datos de la segunda api

---
+++ pages/1_app1.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def main():
+   st.title("API de registros de horarios")
+   st.write("Esta API permite gestionar los registros de entrada y salida de los empleados de una empresa.")
+
+
+   if __name__ == "__main__":
+       main()
--- pages/2_CSV.py
+++ pages/2_CSV.py
@@ -1,1 +1,6 @@
 ## Aplicación 2 para un registro de un api
+import streamlit as st
+import pandas as pd
+datos = pd.read_csv('pages\WL_B_Aeropuertos.csv')
+st.title("Registro de Aeropuertos ✈")
+st.dataframe(datos)   

6f31fbddb9055b1834107127f11a618f0511901b
Samuel <samuelbernal0809@gmail.com>
2025-09-17 14:18:03 -0500

    modificacio de diseño en la app

--- pages/1_app1.py
+++ pages/1_app1.py
@@ -2,17 +2,10 @@ import streamlit as st
 from proyecto import mostrar_horarios
 from aeropuertos import mostrar_aeropuertos
 
-st.header("Panel Principal")
-
-# Selector en la barra lateral
-opcion = st.sidebar.radio(
-    "Selecciona la vista:",
-    ("Horarios", "Aeropuertos")
-)
+st.sidebar.title("Menú")
+opcion = st.sidebar.selectbox("Selecciona una opción", ["Horarios", "Aeropuertos"])
 
 if opcion == "Horarios":
-    st.subheader("Sección Horarios")
     mostrar_horarios()
 elif opcion == "Aeropuertos":
-    st.subheader("Sección Aeropuertos")
     mostrar_aeropuertos()
